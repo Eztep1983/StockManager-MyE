d516d7a76a5ec9fa09cbc4d93ccad2b0554ed0ac
Eztep1983 <extep1983@gmail.com>
2024-10-18 19:11:51 -0500

    Revision general y correcciones a productos y clientes

--- src/models/ventas.py
+++ src/models/ventas.py
@@ -8,24 +8,23 @@ development_config = config['development']
 
 
 class Venta:
-    def __init__(self, id, cedula_cliente, id_producto, id_usuario, cantidad, fecha_venta):
+    def __init__(self, id, id_usuario, cantidad, fecha_venta, id_cliente):
         self.id = id
-        self.cedula_cliente = cedula_cliente
-        self.id_producto = id_producto
         self.id_usuario = id_usuario
         self.cantidad = cantidad
         self.fecha_venta = fecha_venta
+        self.id_cliente = id_cliente
 
 
 def obtener_ventas():
     conn = mysql.connection
     cursor = conn.cursor()
-    sql = "SELECT id, cedula_cliente, id_producto, id_usuario, cantidad, fecha_venta FROM ventas"
+    sql = "SELECT id, id_usuario, cantidad, fecha_venta, id_cliente FROM ventas"
     cursor.execute(sql)
     Ventas = []
     for row in cursor.fetchall():
-        id, cedula_cliente, id_producto, id_usuario, cantidad, fecha_venta = row
-        ventas = Ventas(id, cedula_cliente, id_producto, id_usuario, cantidad, fecha_venta)
+        id, id_producto, id_usuario, cantidad, fecha_venta, id_cliente = row
+        ventas = Ventas(id, id_producto, id_usuario, cantidad, fecha_venta, id_cliente)
         Ventas.append(ventas)
     cursor.close()
     return Ventas
