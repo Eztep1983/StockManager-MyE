e78394fab9a953553360816743a26fcb089d2b11
Eztep1983 <extep1983@gmail.com>
2024-11-06 16:53:54 -0500

    deleting db structure to upload a new one

--- src/app.py
+++ src/app.py
@@ -204,7 +204,6 @@ def get_categorias():
 @login_required
 def ventas():
     lista_ventas = obtener_ventas()
-    print(f"Ventas obtenidas: {lista_ventas}")#PARA DEPURAR SI ESTA RECIBIENDO O NO LAS VENTAS 
     return render_template('ventas.html', ventas=lista_ventas)
 
 
